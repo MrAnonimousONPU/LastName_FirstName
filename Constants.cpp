3cf2a36040cd3b68a30a777144d1bdccbd2e90b5
Valentine <mudrikval@gmail.com>
2020-01-09 21:38:48 +0200

    Added constants
    Added constants for easy readability, and operation

---
+++ Constants.cpp
@@ -0,0 +1,91 @@
+#include "Constants.h"
+
+int getColorBlack()
+{
+ return 0;
+}
+
+int getColorBlue()
+{
+ return 9;
+}
+
+int getColorCyan()
+{
+ return 11;
+}
+
+int getColorRed()
+{
+ return 12;
+}
+
+int getColorOrange()
+{
+ return 6;
+}
+
+int getColorYellow()
+{
+ return 14;
+}
+
+int getColorWhite()
+{
+ return 15;
+}
+
+int getMaxNumLives()
+{
+ return 3;
+}
+
+int getStartX()
+{
+	return 3;
+}
+
+int getStartY()
+{
+	return 3;
+}
+
+int getInfoScoreFieldHeight()
+{
+ return 3;
+}
+
+int getInfoLivesFieldHeight()
+{
+	return 1;
+}
+
+int getPlayingFieldWidth()
+{
+ return 28;
+}
+
+int getPlayingFieldHeight()
+{
+ return 31;
+}
+
+int getDirectionUp()
+{
+ return 0;
+}
+
+int getDirectionDown()
+{
+ return 1;
+}
+
+int getDirectionRight()
+{
+ return 2;
+}
+
+int getDirectionLeft()
+{
+ return 3;
+}
---
+++ Constants.h
@@ -0,0 +1,23 @@
+#pragma once
+
+int getColorBlack();
+int getColorBlue();
+int getColorCyan();
+int getColorRed();
+int getColorOrange();
+int getColorYellow();
+int getColorWhite();
+
+int getMaxNumLives();
+int getStartX();
+int getStartY();
+
+int getInfoScoreFieldHeight();
+int getInfoLivesFieldHeight();
+int getPlayingFieldWidth();
+int getPlayingFieldHeight();
+
+int getDirectionUp();
+int getDirectionDown();
+int getDirectionRight();
+int getDirectionLeft();
